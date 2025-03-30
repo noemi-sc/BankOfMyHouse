773089b3d870a535664b47dcc47fe2e6cf66efe3
Noemi Scata <noemiscata96@gmail.com>
2025-03-30 19:10:50 +0200

    Added JwtConfiguration class to leverage Options pattern and appsettings for DB

---
+++ BankOfMyHouse.Server/BankOfMyHouse.Infrastructure/Configurations/JwtConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace BankOfMyHouse.Infrastructure.Configurations;
+
+public class JwtConfiguration
+{
+    public const string ConfigurationSection = "Jwt";
+
+    public required string Issuer { get; init; }
+    public required string Audience { get; init; }
+    public required string Secret { get; init; }
+}
