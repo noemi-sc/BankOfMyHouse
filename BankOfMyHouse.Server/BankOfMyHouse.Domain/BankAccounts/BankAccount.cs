8b029efd5d00fe571452addb115a7fefc72c9d23
Noemi Scata <noemiscata96@gmail.com>
2025-03-30 19:08:43 +0200

    Added bankAccount and ibanCode as domain classes

---
+++ BankOfMyHouse.Server/BankOfMyHouse.Domain/BankAccounts/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿using BankOfMyHouse.Domain.Iban;
+using BankOfMyHouse.Domain.Iban.Interfaces;
+
+namespace BankOfMyHouse.Domain.BankAccounts;
+
+public class BankAccount
+{
+    public Guid Id { get; init; }
+    public IbanCode IBAN { get; init; }
+    public string UserId { get; init; }
+    public DateTimeOffset CreationDate { get; init; }
+
+    // Navigation property
+    public ApplicationUser User { get; init; } // Add this property
+
+    // Parameterless constructor for EF Core
+    private BankAccount() { }
+
+    private BankAccount(string userId, IbanCode iban)
+    {
+        Id = Guid.NewGuid();
+        UserId = userId;
+        IBAN = iban;
+        CreationDate = DateTimeOffset.Now;
+    }
+
+    public static BankAccount CreateNew(string userId, IIbanGenerator ibanGenerator)
+    {
+        if (string.IsNullOrWhiteSpace(userId))
+            throw new ArgumentException("User ID cannot be empty", nameof(userId));
+
+        var iban = ibanGenerator.GenerateItalianIban();
+        return new BankAccount(userId, iban);
+    }
+
+    // Factory method for reconstruction from persistence
+    public static BankAccount Reconstitute(string userId, IbanCode iban)
+    {
+        return new BankAccount(userId, iban);
+    }
+}
---
+++ BankOfMyHouse.Server/BankOfMyHouse.Domain/Iban/IbanCode.cs
@@ -0,0 +1,19 @@
+﻿namespace BankOfMyHouse.Domain.Iban;
+
+public sealed record IbanCode
+{
+    public string Value { get; }
+
+    private IbanCode(string value) => Value = value;
+
+    public static IbanCode Create(string iban)
+    {
+        if (string.IsNullOrWhiteSpace(iban))
+            throw new Exception("IBAN cannot be empty");
+
+        //if (!ItalianIbanValidator.IsValid(iban))
+        //    return Result.Failure<IbanCode>("Invalid IBAN format");
+
+        return new IbanCode(iban);
+    }
+}
