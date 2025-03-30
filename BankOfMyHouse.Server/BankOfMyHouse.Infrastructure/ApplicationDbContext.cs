0d6f4c6f6237c712854a09c93db0a3bbb9af0b50
Noemi Scata <noemiscata96@gmail.com>
2025-03-30 19:10:01 +0200

    Added ApplicationDbContext that leverages EF Core as ORM

---
+++ BankOfMyHouse.Server/BankOfMyHouse.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BankOfMyHouse.Domain.BankAccounts;
+using BankOfMyHouse.Infrastructure.Configurations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankOfMyHouse.Infrastructure;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public DbSet<BankAccount> BankAccounts { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new BankAccountConfiguration());
+    }
+}
