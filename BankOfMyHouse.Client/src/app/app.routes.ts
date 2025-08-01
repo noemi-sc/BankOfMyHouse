543284266dca7a7cbc42c6bedcfd16b629b46045
Noemi Scata <nopetrova96.2@gmail.com>
2025-08-01 13:37:26 +0200

    Added routes to application ofr login and register

--- BankOfMyHouse.Client/src/app/app.routes.ts
+++ BankOfMyHouse.Client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { GuestGuard } from './auth/guards/guest.guard';
+import { AuthGuard } from './auth/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [GuestGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [GuestGuard] },
+  { path: '**', redirectTo: '/login' },
+  // Add more routes as needed
+];
