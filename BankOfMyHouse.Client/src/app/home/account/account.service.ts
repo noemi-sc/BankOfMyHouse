c4f66a01324a3b3438b7dbd1633f373759b508ea
Noemi Scata <noemiscata96@gmail.com>
2025-08-09 19:43:53 +0200

    Added service for bankAccount related endpoints

--- BankOfMyHouse.Client/src/app/home/account/account.service.ts
+++ BankOfMyHouse.Client/src/app/home/account/account.service.ts
@@ -1,8 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  
+  private apiUrl = 'http://localhost:57460/bankAccounts';
+
+  constructor(
+    private httpClient: HttpClient
+  ) {
+    this.httpClient = httpClient;
+  }
+
+  listAccounts(): Observable<any> {
+    return this.httpClient.get(`${this.apiUrl}`);
+  }
 }
