120c782dd916d9bfc44b43d83af33cf5086a49d7
Noemi Scata <noemiscata96@gmail.com>
2025-08-09 19:40:38 +0200

    Added charts imports to main.ts

--- BankOfMyHouse.Client/src/main.ts
+++ BankOfMyHouse.Client/src/main.ts
@@ -1,5 +1,30 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {
+    Chart,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    TimeScale,
+    LineController,
+} from 'chart.js';
+import 'chartjs-adapter-date-fns';
+
+Chart.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    TimeScale,    
+    LineController
+);
 
 bootstrapApplication(AppComponent, appConfig);
