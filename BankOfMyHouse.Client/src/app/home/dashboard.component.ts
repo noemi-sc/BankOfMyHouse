dcf07f42787de3c712595f5968ccf1393fbc001b
Noemi Scata <noemiscata96@gmail.com>
2025-08-21 12:55:58 +0200

    added Popup for payment, modified popup functions, moved account details in first field

--- BankOfMyHouse.Client/src/app/home/dashboard.component.ts
+++ BankOfMyHouse.Client/src/app/home/dashboard.component.ts
@@ -1,48 +1,110 @@
 // components/dashboard/dashboard.component.ts
-import { Component, computed, ChangeDetectionStrategy, inject, signal } from '@angular/core';
-import { CurrencyPipe, DecimalPipe } from '@angular/common';
-import { UserDto } from '../auth/models/user';
-/* import { DashboardService } from '../../services/dashboard.service';
-import { AccountCardComponent } from '../account-card/account-card.component';
-import { InvestmentItemComponent } from '../investment-item/investment-item.component';
-import { TransactionItemComponent } from '../transaction-item/transaction-item.component';
-import { MarketOverviewComponent } from '../market-overview/market-overview.component'; */
+import { Component, computed, ChangeDetectionStrategy, inject, signal, OnInit, ViewEncapsulation } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, DatePipe } from '@angular/common';
+import { UsersService } from '../users/users.service';
+import { Router } from '@angular/router';
+import { GetUserDetailsResponseDto } from '../auth/models/getUserDetails/getUserDetailsResponseDto';
+import { BankAccountService } from '../bank-account.service';
+import { CreateBankAccountComponent } from '../create-bank-account/create-bank-account.component';
+import { CreateTransactionComponent } from '../transactions/create-transaction/create-transaction.component';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-dashboard',
   changeDetection: ChangeDetectionStrategy.OnPush,
-    templateUrl: './dashboard.component.html',
+  templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
   standalone: true,
   imports: [
     CurrencyPipe,
     DecimalPipe,
-/*     AccountCardComponent,
-    InvestmentItemComponent,
-    TransactionItemComponent,
-    MarketOverviewComponent */
+    DatePipe,
+    CreateBankAccountComponent,
+    CreateTransactionComponent,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule
+    /*     AccountCardComponent,
+        InvestmentItemComponent,
+        TransactionItemComponent,
+        MarketOverviewComponent */
   ],
 
 
 })
-export class DashboardComponent {
-  // State signals
-  currentUser = signal<UserDto | null>({
-    email: 'John Doe',
-    username: '1234567890',
-    createdAt: '2023-10-01T12:00:00Z',
-    isActive: true,
-    roles: ['User'],
+export class DashboardComponent implements OnInit {
 
-  });
+  private usersService = inject(UsersService);
+  private router = inject(Router);
+  private bankAccountService = inject(BankAccountService);
+  protected readonly isBankAccountPopupOpen = signal(false);
+  protected readonly isTransactionPopupOpen = signal(false);
+  protected readonly inputValue = signal('');
+
+  // State signals from service
+  currentUser = this.usersService.userDetails;
+  loading = this.usersService.loading;
+  error = this.usersService.error;
+
+  
+
+
+  ngOnInit() {
+    this.usersService.getUserDetails();
+  }
   // dashboardService = inject(DashboardService);
-  
-  // portfolioChange = computed(() => this.dashboardService.portfolioChange());
-  
-/*   recentTransactions = computed(() => 
-    this.dashboardService.transactionsData()
-      .slice()
-      .sort((a, b) => b.date.getTime() - a.date.getTime())
-      .slice(0, 5)
-  ); */
+
+//   portfolioChange = computed(() => this.dashboardService.portfolioChange());
+
+  /*   recentTransactions = computed(() => 
+      this.dashboardService.transactionsData()
+        .slice()
+        .sort((a, b) => b.date.getTime() - a.date.getTime())
+        .slice(0, 5)
+    ); */
+
+
+  // Popup for new IBAN
+  protected openBankAccountPopup(): void {
+    this.isBankAccountPopupOpen.set(true);
+  }
+
+  protected onBankAccountPopupConfirmed(value: string): void {
+    console.log('Bank account confirmed with value:', value);
+    this.inputValue.set(value);
+    this.isBankAccountPopupOpen.set(false);
+  }
+
+  protected onBankAccountPopupCancelled(): void {
+    console.log('Bank account popup cancelled');
+    this.isBankAccountPopupOpen.set(false);
+  }
+
+  // Popup for new payment
+
+  protected openTransactionPopup(): void {
+    this.isTransactionPopupOpen.set(true);
+  }
+
+  protected onTransactionPopupConfirmed(value: string): void {
+    console.log('Transaction confirmed with value:', value);
+    this.inputValue.set(value);
+    this.isTransactionPopupOpen.set(false);
+  }
+
+  protected onTransactionPopupCancelled(): void {
+    console.log('Transaction popup cancelled');
+    this.isTransactionPopupOpen.set(false);
+  }
+
+
+
 }
