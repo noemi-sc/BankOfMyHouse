e7986edf47190ae50f276e0156171b95173682e4
Noemi Scata <noemiscata96@gmail.com>
2025-08-27 22:33:20 +0200

    Added FromQuery attribute to requestDto

--- BankOfMyHouse.Server/BankOfMyHouse.API/Endpoints/Transactions/GetTransactions/GetTransactionsRequestDto.cs
+++ BankOfMyHouse.Server/BankOfMyHouse.API/Endpoints/Transactions/GetTransactions/GetTransactionsRequestDto.cs
@@ -1,9 +1,14 @@
-﻿namespace BankOfMyHouse.API.Endpoints.Transactions.GetTransactions
+﻿using FastEndpoints;
+
+namespace BankOfMyHouse.API.Endpoints.Transactions.GetTransactions
 {
 	public sealed record GetTransactionsRequestDto
 	{
-		public DateTimeOffset StartDate { get; set; }
-		public DateTimeOffset EndDate { get; set; }
+		[FromQuery]
+		public DateTimeOffset StartDate { get; set; } = DateTimeOffset.UtcNow.AddDays(-7);
+		[FromQuery]
+		public DateTimeOffset EndDate { get; set; } = DateTimeOffset.UtcNow;
+		[FromQuery]
 		public string Iban { get; set; }
 	}
 }
