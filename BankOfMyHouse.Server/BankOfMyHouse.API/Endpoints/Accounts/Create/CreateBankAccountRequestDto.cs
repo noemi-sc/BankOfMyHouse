b4f26a0f472d06d9d2c41cd59176c928f064d5c7
Noemi Scata <noemiscata96@gmail.com>
2025-08-27 23:12:26 +0200

    Added claim requirement

--- BankOfMyHouse.Server/BankOfMyHouse.API/Endpoints/Accounts/Create/CreateBankAccountRequestDto.cs
+++ BankOfMyHouse.Server/BankOfMyHouse.API/Endpoints/Accounts/Create/CreateBankAccountRequestDto.cs
@@ -1,7 +1,11 @@
-﻿namespace BankOfMyHouse.API.Endpoints.Accounts.Create;
+﻿using FastEndpoints;
+
+namespace BankOfMyHouse.API.Endpoints.Accounts.Create;
 
 
 public sealed record CreateBankAccountRequestDto
 {
+	[FromClaim("userId")]
+	public required int UserId { get; set; }
 	public string Description { get; set; } = string.Empty;
 }
