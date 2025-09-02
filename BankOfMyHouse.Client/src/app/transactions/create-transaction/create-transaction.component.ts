dfbb2c16866defce29b3df983746dc235f7e4bae
Noemi Scata <noemiscata96@gmail.com>
2025-09-02 19:49:23 +0200

    modified the input fields

--- BankOfMyHouse.Client/src/app/transactions/create-transaction/create-transaction.component.ts
+++ BankOfMyHouse.Client/src/app/transactions/create-transaction/create-transaction.component.ts
@@ -4,16 +4,16 @@ import { BankAccountService } from '../../bank-account.service';
 import { CreateTransactionRequestDto, IbanCodeDto } from '../models/createTransactionRequestDto';
 import { TransactionService } from '../transaction.service';
 import { UsersService } from '../../users/users.service';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-create-transaction',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './create-transaction.component.html',
-  styleUrl: './create-transaction.component.css'
+  styleUrls: ['./create-transaction.component.css', '../create-transaction-custom-style.scss']
 })
 export class CreateTransactionComponent {
   // Inputs
@@ -28,10 +28,17 @@ export class CreateTransactionComponent {
   protected readonly currentValue = signal('');
   protected readonly selectedSenderIban = signal<any>(null);
   protected readonly receiverIban = signal('');
-  protected readonly amount = signal(0);
+  protected readonly amount = signal<number | null>(null);
+  protected readonly currencies = [
+    { name: "US Dollar", code: "USD", symbol: "$" },
+    { name: "Euro", code: "EUR", symbol: "€" },
+    { name: "British Pound", code: "GBP", symbol: "£" }
+  ];
+    protected readonly selectedCurrency = signal<any>(this.currencies[1].code);
 
   private transactionService = inject(TransactionService);
   private usersService = inject(UsersService);
+
 
   // State signals from service
   currentUser = this.usersService.userDetails;
@@ -52,17 +59,28 @@ export class CreateTransactionComponent {
     if (this.selectedSenderIban()) {
       requestBody.senderIban = new IbanCodeDto(this.selectedSenderIban().iban);
     }
-    
+
     // Set receiver IBAN from form input
     requestBody.receiverIban = new IbanCodeDto(this.receiverIban());
-    
+
     // Set amount
-    requestBody.amount = this.amount();
-    
+    requestBody.amount = this.amount() ?? 0;
+
+      // Set currency
+    requestBody.currencyCode  = this.selectedCurrency().code;
+
     // Set description (causale)
     requestBody.description = this.currentValue();
 
-    this.transactionService.createTransaction(requestBody);
+    this.transactionService.createTransaction(requestBody).subscribe({
+      next: (response) => {
+        // Transaction created successfully, emit success event
+        this.cancelled.emit(); // Close the popup
+      },
+      error: (error) => {
+        console.error('Transaction creation failed:', error);
+      }
+    });
   }
 
   protected onCancel(): void {
