94bb64a5e83d38ef95a8e64f230709db8aa23d33
Noemi Scata <noemiscata96@gmail.com>
2025-10-26 15:47:02 +0100

    minor refactor to implement signals instead and fixed issue that wasn't reloading the list of bank accounts

--- BankOfMyHouse.Client/src/app/dashboard/finance_dashboard/dashboard.component.ts
+++ BankOfMyHouse.Client/src/app/dashboard/finance_dashboard/dashboard.component.ts
@@ -1,17 +1,13 @@
 // components/dashboard/dashboard.component.ts
-import { Component, computed, ChangeDetectionStrategy, inject, signal, OnInit, Signal } from '@angular/core';
-import { CurrencyPipe, DecimalPipe, DatePipe } from '@angular/common';
+import { Component, computed, ChangeDetectionStrategy, inject, signal, OnInit } from '@angular/core';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 import { UserService } from '../../services/users/users.service';
-import { Router } from '@angular/router';
-import { BankAccountService } from '../../account/bank-account.service';
 import { CreateTransactionComponent } from '../../transactions/create/create-transaction.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { GetTransactionComponent } from "../../transactions/list/get-transaction.component";
 import { MatGridListModule } from '@angular/material/grid-list';
-import { TransactionService } from '../../transactions/transaction.service';
-import { GetUserDetailsResponseDto } from '../../auth/models/getUserDetails/getUserDetailsResponseDto';
 import { CreateBankAccountComponent } from '../../account/create/create-bank-account.component';
 
 @Component({
@@ -32,23 +28,18 @@ import { CreateBankAccountComponent } from '../../account/create/create-bank-acc
     GetTransactionComponent,
     MatGridListModule
   ],
-
-
 })
 export class DashboardComponent implements OnInit {
 
   private usersService = inject(UserService);
-  private router = inject(Router);
-  private bankAccountService = inject(BankAccountService);
-  private transactionService = inject(TransactionService);
+
   protected readonly isBankAccountPopupOpen = signal(false);
   protected readonly isTransactionPopupOpen = signal(false);
-  protected readonly inputValue = signal('');
 
   // State signals from service
-  protected currentUser: Signal<GetUserDetailsResponseDto | null>;
-  loading = this.usersService.loading;
-  error = this.usersService.error;
+  protected currentUser = this.usersService.userDetails;
+  protected loading = this.usersService.loading;
+  protected error = this.usersService.error;
 
   // Computed signal for total balance of all accounts
   totalBalance = computed(() => {
@@ -56,14 +47,10 @@ export class DashboardComponent implements OnInit {
     return this.currentUser()?.bankAccounts.reduce((sum, account) => sum + account.balance, 0);
   });
 
-  totalAccounts = computed(() => {
+  protected totalAccounts = computed(() => {
     if (!this.currentUser()?.bankAccounts) return 0;
     return this.currentUser()?.bankAccounts.length;
   });
-
-  constructor() {
-    this.currentUser = this.usersService.userDetails;
-  }
 
   ngOnInit(): void {
     this.usersService.getUserDetails();
@@ -74,31 +61,28 @@ export class DashboardComponent implements OnInit {
     this.isBankAccountPopupOpen.set(true);
   }
 
-  protected onBankAccountPopupConfirmed(value: string): void {
-    console.log('Bank account confirmed with value:', value);
-    this.inputValue.set(value);
+  protected onBankAccountPopupConfirmed(): void {
     this.isBankAccountPopupOpen.set(false);
+    // Refresh user details to show the newly created bank account
+    this.usersService.getUserDetails();
   }
 
   protected onBankAccountPopupCancelled(): void {
-    console.log('Bank account popup cancelled');
     this.isBankAccountPopupOpen.set(false);
   }
 
   // Popup for new payment
-
   protected openTransactionPopup(): void {
     this.isTransactionPopupOpen.set(true);
   }
 
-  protected onTransactionPopupConfirmed(value: string): void {
-    console.log('Transaction confirmed with value:', value);
-    this.inputValue.set(value);
+  protected onTransactionPopupConfirmed(): void {
     this.isTransactionPopupOpen.set(false);
+    // Refresh user details to show updated balances after transaction
+    this.usersService.getUserDetails();
   }
 
   protected onTransactionPopupCancelled(): void {
-    console.log('Transaction popup cancelled');
     this.isTransactionPopupOpen.set(false);
   }
 }
