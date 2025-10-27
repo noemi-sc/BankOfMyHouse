2b5908009f250d95a9d478cd8a592a4c7050a38f
Noemi Scata <noemiscata96@gmail.com>
2025-10-27 12:58:17 +0100

    renamed folder for better clarity

--- BankOfMyHouse.Client/src/app/app.routes.ts
+++ BankOfMyHouse.Client/src/app/app.routes.ts
@@ -4,12 +4,12 @@ import { RegisterComponent } from './auth/register/register.component';
 import { GuestGuard } from './auth/guards/guest.guard';
 import { AuthGuard } from './auth/guards/auth.guard';
 import { DashboardComponent } from './dashboard/finance_dashboard/dashboard.component';
-import { InvestmentComponent } from './investment/investment.component';
+import { InvestmentsDashboardComponent } from './dashboard/investments_dashboard/investments-dashboard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'investments', component: InvestmentComponent, canActivate: [AuthGuard] },
+  { path: 'investments', component: InvestmentsDashboardComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent, canActivate: [GuestGuard] },
   { path: 'register', component: RegisterComponent, canActivate: [GuestGuard] },
   { path: '**', redirectTo: '/login' },
